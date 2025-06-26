ea016dd1937d1c904ddac380b2d697f749007f24
tamaz gociridze <tazogociridze9@gmail.com>
2025-06-26 17:55:59 +0400

    simple changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-      <div>
-
-      </div>
-  )
+    <main>
+      <header className="h-[101px] bg-[var(--main-color-light)] flex items-center justify-between">
+        <div className="logo !ml-5"></div>
+        <nav className="flex gap-x-[30px] text-[25px] text-[var(--mail-color-dark)] ">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Courses</a>
+          <a href="#">Contact</a>
+        </nav>
+      </header>
+    </main>
+  );
 }
 
-export default App
+export default App;
